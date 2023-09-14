9faccf4946b9416990595e8f71414f091ecf5928
PamuduC <pamudupamudu@gmail.com>
2023-09-15 00:14:24 +0530

    fix register error

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,11 @@
-import React, { useEffect,useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import '../styles/login.css';
-import login from '../components/images/login.jpg';
-import { GoogleLogin } from 'react-google-login';
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import "../styles/login.css";
+import login from "../components/images/login.jpg";
+import swal from "sweetalert";
+
+import { GoogleLogin } from "react-google-login";
 import {
   TextField,
   Grid,
@@ -16,43 +18,45 @@ import {
   Button,
   Alert,
   Snackbar,
-} from '@mui/material';
+} from "@mui/material";
 
 const clientId =
-  '790433585929-p9slfbpl44uau7urp5tu91b5h5trl21j.apps.googleusercontent.com';
+  "790433585929-p9slfbpl44uau7urp5tu91b5h5trl21j.apps.googleusercontent.com";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [open, setOpen] = React.useState(false);
-  const [textInputErrorMessageEmail, setTextInputErrorMessageEmail] = useState(null);
+  const [textInputErrorMessageEmail, setTextInputErrorMessageEmail] =
+    useState(null);
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    try{
-      const response = await axios.post('http://localhost:4000/api/logincustomer/', {
-        email,
-        password,
-      });
-      console.log(response.data, 'Login Success');
-      if(response.data){
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/api/logincustomer/",
+        {
+          email,
+          password,
+        }
+      );
+      console.log(response.data, "Login Success");
+      if (response.data) {
         setOpen(true);
-        localStorage.setItem('token', response.data.token);
-        setEmail('');
-        setPassword('');
-        navigate('/AdminDashboard');
+        localStorage.setItem("token", response.data.token);
+        setEmail("");
+        setPassword("");
+        navigate("/AdminDashboard");
         window.location.reload();
       }
-
-    }catch(error){
-      swal('Invalid Credential!','', 'error'); // Show success message
-      setEmail('');
-      setPassword('');
-
+    } catch (error) {
+      swal("Invalid Credential!", "", "error"); // Show success message
+      setEmail("");
+      setPassword("");
     }
   };
-  
+
   const handleClose = (event, reason) => {
     if (reason === "clickaway") {
       return;
@@ -61,11 +65,11 @@ const Login = () => {
   };
 
   const onSuccess = (res) => {
-    console.log('LOGIN SUCCESS! Current user: ', res.profileobj);
+    console.log("LOGIN SUCCESS! Current user: ", res.profileobj);
   };
 
   const onFailure = (res) => {
-    console.log('LOGIN FAILED! res: ', res);
+    console.log("LOGIN FAILED! res: ", res);
   };
 
   const handleEmailChange = (e) => {
@@ -75,22 +79,19 @@ const Login = () => {
     const emailRegex = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
 
     // Check if the input matches the pattern or is empty
-    if (emailRegex.test(inputValue) || inputValue !== ' ') {
+    if (emailRegex.test(inputValue) || inputValue !== " ") {
       setEmail(inputValue);
+    } else {
+      setTextInputErrorMessageEmail("*Please enter a valid email address");
     }
-    else{
-      setTextInputErrorMessageEmail('*Please enter a valid email address');
-      
-    }
-
   };
   return (
     <Container>
-      <div className='login__customer__container '>
+      <div className="login__customer__container ">
         <Grid container spacing={2}>
           {/* Left side with register customer image */}
           <Grid item xs={12} md={6}>
-            <div className='login__customer__img'>
+            <div className="login__customer__img">
               <img src={login}></img>
             </div>
           </Grid>
@@ -98,11 +99,11 @@ const Login = () => {
           {/* Right side with form components */}
           <Grid item xs={12} md={6}>
             {/* Form */}
-            <div className='login__customer__form'>
+            <div className="login__customer__form">
               <form onSubmit={handleLogin}>
                 <Grid item xs={12}>
                   {/* Heading */}
-                  <div className='login__customer__heading'>
+                  <div className="login__customer__heading">
                     <h2>Login</h2>
                   </div>
                 </Grid>
@@ -110,85 +111,84 @@ const Login = () => {
                   <Grid item xs={12}>
                     {/* Email */}
                     <TextField
-                      className='input-text-login'
-                      id='outlined-basic'
-                      label='Email'
-                      variant='outlined'
+                      className="input-text-login"
+                      id="outlined-basic"
+                      label="Email"
+                      variant="outlined"
                       value={email}
-                      type='text'
+                      type="text"
                       helperText={textInputErrorMessageEmail}
                       onChange={handleEmailChange}
-                      size='small'
-                      style={{ width: '80%' }}
+                      size="small"
+                      style={{ width: "80%" }}
                       required
                     />
                   </Grid>
                   <Grid item xs={12}>
                     {/* Password */}
                     <TextField
-                      className='input-text-login'
-                      id='outlined-password-input'
-                      label='Password'
-                      variant='outlined'
-                      type='password'
+                      className="input-text-login"
+                      id="outlined-password-input"
+                      label="Password"
+                      variant="outlined"
+                      type="password"
                       value={password}
-                      autoComplete='current-password'
-                      size='small'
+                      autoComplete="current-password"
+                      size="small"
                       onChange={(e) => setPassword(e.target.value)}
                       style={{
-                        width: '80%',
-                        marginBottom: '5%',
-                        height: '5%',
+                        width: "80%",
+                        marginBottom: "5%",
+                        height: "5%",
                       }}
                       required
                     />
                   </Grid>
-                  <Grid item xs={12} style={{ width: '70%' }}>
+                  <Grid item xs={12} style={{ width: "70%" }}>
                     {/* Signup Button */}
-                    <div className='login__vehicle__submitbtn'>
-                      <Button type='submit' variant='contained'>
+                    <div className="login__vehicle__submitbtn">
+                      <Button type="submit" variant="contained">
                         Login
                       </Button>
                     </div>
                   </Grid>
-                  <Grid item xs={6} style={{ width: '70%' }}>
+                  <Grid item xs={6} style={{ width: "70%" }}>
                     {/* Forgot Password */}
-                    <div className='forgot__password'>
-                      <a href='/forgot-password'>Forgot Password?</a>
+                    <div className="forgot__password">
+                      <a href="/forgot-password">Forgot Password?</a>
                     </div>
                   </Grid>
-                  <Grid item xs={6} style={{ width: '70%' }}>
+                  <Grid item xs={6} style={{ width: "70%" }}>
                     {/* Remember Me */}
-                    <div className='option_div'>
-                      <div className='remember__me'>
-                        <input type='checkbox' />
+                    <div className="option_div">
+                      <div className="remember__me">
+                        <input type="checkbox" />
                         <span> Remember me</span>
                       </div>
                     </div>
                   </Grid>
 
-                  <Grid item xs={12} style={{ width: '80%' }}>
+                  <Grid item xs={12} style={{ width: "80%" }}>
                     {/* Google Signup Button */}
-                    <div className='social__icons__login' id='signInDutton'>
+                    <div className="social__icons__login" id="signInDutton">
                       <p> or Sign Up Using</p>
 
                       <GoogleLogin
-                        className='google__login'
+                        className="google__login"
                         clientId={clientId}
-                        buttonText='Sign in with Google'
+                        buttonText="Sign in with Google"
                         onSuccess={onSuccess}
                         onFailure={onFailure}
-                        cookiePolicy={'single_host_origin'}
+                        cookiePolicy={"single_host_origin"}
                         isSignedIn={true}
                       />
                     </div>
                   </Grid>
-                  <Grid item xs={12} style={{ width: '100%' }}>
+                  <Grid item xs={12} style={{ width: "100%" }}>
                     {/* move to signup */}
-                    <div className='already__have__an__account'>
+                    <div className="already__have__an__account">
                       <p>
-                        Don't have an account?{' '}
-                        <Link to='/signup'>Create</Link>
+                        Don't have an account? <Link to="/signup">Create</Link>
                       </p>
                     </div>
                   </Grid>
--- frontend/src/pages/dashboard/services/RegisterVehicle.js
+++ frontend/src/pages/dashboard/services/RegisterVehicle.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import '../../../styles/registervehicle.css';
-import { Box } from '@mui/material';
-import Topbar from '../components/Navbar';
-import Sidebar from '../components/Sidebar';
-import registerVehicle from '../../../components/images/registerVehicle.jpg';
+import React, { useState } from "react";
+import "../../../styles/registervehicle.css";
+import { Box } from "@mui/material";
+import Topbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import registerVehicle from "../../../components/images/registerVehicle.jpg";
 import {
   TextField,
   Grid,
@@ -14,20 +14,19 @@ import {
   Select,
   InputAdornment,
   Button,
-  Alert,
-  Snackbar,
-} from '@mui/material';
-import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import dayjs from 'dayjs';
+} from "@mui/material";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
+import swal from "sweetalert";
 
 const RegisterVehicle = () => {
-  const [registerNumber, setRegisterNumber] = useState('');
-  const [chassisFirstCode, setChasisFirstcode] = useState('');
-  const [make, setMake] = useState('');
-  const [model, setModel] = useState('');
+  const [registerNumber, setRegisterNumber] = useState("");
+  const [chassisFirstCode, setChasisFirstcode] = useState("");
+  const [make, setMake] = useState("");
+  const [model, setModel] = useState("");
   const [lastServiceDate, setLastServiceDate] = useState(dayjs());
-  const [fuelType, setFuelType] = useState('');
+  const [fuelType, setFuelType] = useState("");
   const [lastServiceMileage, setLastServiceMileage] = useState(0);
   const [open, setOpen] = React.useState(false);
 
@@ -38,7 +37,7 @@ const RegisterVehicle = () => {
       chassisFirstCode,
       make,
       model,
-      lastServiceDate.format('YYYY-MM-DD'),
+      lastServiceDate.format("YYYY-MM-DD"),
       fuelType,
       lastServiceMileage
     );
@@ -47,15 +46,15 @@ const RegisterVehicle = () => {
       chassisFirstCode,
       make,
       model,
-      lastServiceDate: lastServiceDate.format('YYYY-MM-DD'),
+      lastServiceDate: lastServiceDate.format("YYYY-MM-DD"),
       fuelType,
       lastServiceMileage,
     };
-    const response = await fetch('api/registervehicle/', {
-      method: 'POST',
+    const response = await fetch("http://localhost:3000/api/registervehicle/", {
+      method: "POST",
       body: JSON.stringify(vehicle),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
 
@@ -63,23 +62,24 @@ const RegisterVehicle = () => {
 
     if (!response.ok) {
       console.log(json);
-      console.log('Error Registering the vehicle');
+      console.log("Error Registering the vehicle");
     }
 
     if (response.ok) {
-      setRegisterNumber('');
-      setChasisFirstcode('');
-      setMake('');
-      setModel('');
+      setRegisterNumber("");
+      setChasisFirstcode("");
+      setMake("");
+      setModel("");
       setLastServiceDate(dayjs());
-      setFuelType('');
+      setFuelType("");
       setLastServiceMileage(0);
       setOpen(true);
+      swal("Registered vehicle!", "", "success");
     }
   };
 
   const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
     setOpen(false);
@@ -92,7 +92,7 @@ const RegisterVehicle = () => {
     const pattern = /^[0-9]+(\.[0-9]*)?$/;
 
     // Check if the input matches the pattern or is empty
-    if (pattern.test(inputValue) || inputValue !== ' ') {
+    if (pattern.test(inputValue) || inputValue !== " ") {
       setLastServiceMileage(inputValue);
     }
   };
@@ -101,15 +101,15 @@ const RegisterVehicle = () => {
     <>
       <Topbar />
       <Box height={50} />
-      <Box sx={{ display: 'flex' }}>
+      <Box sx={{ display: "flex" }}>
         <Sidebar />
-        <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
           <Container>
-            <div className='register__vehicle__container '>
+            <div className="register__vehicle__container ">
               <Grid container spacing={2}>
                 {/* Left side with register vehicle image */}
                 <Grid item xs={12} md={6}>
-                  <div className='register__vehicle__img'>
+                  <div className="register__vehicle__img">
                     <img src={registerVehicle}></img>
                   </div>
                 </Grid>
@@ -117,66 +117,66 @@ const RegisterVehicle = () => {
                 {/* Right side with form components */}
                 <Grid item xs={12} md={6}>
                   {/* Form */}
-                  <div className='register__vehicle__form'>
+                  <div className="register__vehicle__form">
                     <form onSubmit={handleSubmit}>
                       <Grid container spacing={4}>
                         {/* Register Number */}
                         <Grid item xs={12}>
                           <TextField
-                            className='input-text'
-                            id='outlined-basic'
-                            label='Register Number'
-                            variant='outlined'
+                            className="input-text"
+                            id="outlined-basic"
+                            label="Register Number"
+                            variant="outlined"
                             value={registerNumber}
                             onChange={(e) => {
                               setRegisterNumber(e.target.value);
                             }}
-                            style={{ width: '100%' }}
+                            style={{ width: "100%" }}
                             required
                           />
                         </Grid>
                         <Grid item xs={12}>
                           {/* Chasis First Code */}
                           <TextField
-                            className='input-text'
-                            id='outlined-basic'
-                            label='Chasis First Code'
-                            variant='outlined'
+                            className="input-text"
+                            id="outlined-basic"
+                            label="Chasis First Code"
+                            variant="outlined"
                             value={chassisFirstCode}
                             onChange={(e) => {
                               setChasisFirstcode(e.target.value);
                             }}
                             required
-                            style={{ width: '100%' }}
+                            style={{ width: "100%" }}
                           />
                         </Grid>
                         <Grid item xs={6}>
                           {/* Make */}
                           <TextField
-                            className='input-text'
-                            id='outlined-basic'
-                            label='Make'
-                            variant='outlined'
+                            className="input-text"
+                            id="outlined-basic"
+                            label="Make"
+                            variant="outlined"
                             value={make}
                             onChange={(e) => {
                               setMake(e.target.value);
                             }}
-                            style={{ width: '100%' }}
+                            style={{ width: "100%" }}
                             required
                           />
                         </Grid>
                         <Grid item xs={6}>
                           {/* Model */}
                           <TextField
-                            className='input-text'
-                            id='outlined-basic'
-                            label='Model'
-                            variant='outlined'
+                            className="input-text"
+                            id="outlined-basic"
+                            label="Model"
+                            variant="outlined"
                             value={model}
                             onChange={(e) => {
                               setModel(e.target.value);
                             }}
-                            style={{ width: '100%' }}
+                            style={{ width: "100%" }}
                             required
                           />
                         </Grid>
@@ -184,58 +184,58 @@ const RegisterVehicle = () => {
                           {/* Select Last Service Date */}
                           <LocalizationProvider dateAdapter={AdapterDayjs}>
                             <DatePicker
-                              label='Last Service Date'
+                              label="Last Service Date"
                               value={lastServiceDate}
                               onChange={(e) => {
                                 setLastServiceDate(e);
                               }}
-                              sx={{ width: '100%' }}
+                              sx={{ width: "100%" }}
                             />
                           </LocalizationProvider>
                         </Grid>
                         <Grid item xs={6}>
                           {/* Select Fuel Type */}
-                          <FormControl style={{ width: '100%' }}>
+                          <FormControl style={{ width: "100%" }}>
                             <InputLabel>Fuel Type</InputLabel>
                             <Select
-                              labelId='demo-simple-select-label'
-                              id='register-vehicle-fueltype'
+                              labelId="demo-simple-select-label"
+                              id="register-vehicle-fueltype"
                               value={fuelType}
-                              label='Fuel Type'
+                              label="Fuel Type"
                               onChange={(e) => {
                                 setFuelType(e.target.value);
                               }}
                               required
                             >
-                              <MenuItem value={'Gasoline'}>Gasoline</MenuItem>
-                              <MenuItem value={'Diesel'}>Diesel</MenuItem>
-                              <MenuItem value={'Electric'}>Electric</MenuItem>
+                              <MenuItem value={"Gasoline"}>Gasoline</MenuItem>
+                              <MenuItem value={"Diesel"}>Diesel</MenuItem>
+                              <MenuItem value={"Electric"}>Electric</MenuItem>
                             </Select>
                           </FormControl>
                         </Grid>
                         <Grid item xs={6}>
                           {/* Last Service Mileage */}
                           <TextField
-                            className='input-text'
-                            id='outlined-basic'
+                            className="input-text"
+                            id="outlined-basic"
                             InputProps={{
                               endAdornment: (
-                                <InputAdornment position='end'>
+                                <InputAdornment position="end">
                                   km
                                 </InputAdornment>
                               ),
                             }}
-                            label='Last Service Mileage'
-                            variant='outlined'
+                            label="Last Service Mileage"
+                            variant="outlined"
                             value={lastServiceMileage}
                             onChange={handleLastServiceMileageChange}
                             required
                           />
                         </Grid>
-                        <Grid item xs={12} style={{ width: '100%' }}>
+                        <Grid item xs={12} style={{ width: "100%" }}>
                           {/* Submit Button */}
-                          <div className='register__vehicle__submitbtn'>
-                            <Button type='submit' variant='contained'>
+                          <div className="register__vehicle__submitbtn">
+                            <Button type="submit" variant="contained">
                               Register
                             </Button>
                           </div>
@@ -246,21 +246,6 @@ const RegisterVehicle = () => {
                 </Grid>
               </Grid>
             </div>
-            {/* Snackbar component */}
-            <Snackbar
-              anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-              open={open}
-              autoHideDuration={2000}
-              onClose={handleClose}
-            >
-              <Alert
-                onClose={handleClose}
-                severity='success'
-                sx={{ width: '100%' }}
-              >
-                Vehicle Registered Successfully
-              </Alert>
-            </Snackbar>
           </Container>
         </Box>
       </Box>
