c63d2f2ed9123ea6fbc009df02e8dcafc23d4c5c
Pamudu Chathunka <119758027+pamuduchat@users.noreply.github.com>
2023-10-24 22:49:18 +0530

    fix sidebar

--- frontend/src/pages/customerDash/components/Sidebar.js
+++ frontend/src/pages/customerDash/components/Sidebar.js
@@ -1,74 +1,74 @@
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MuiDrawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import { useNavigate } from 'react-router-dom';
-import { useAppStore } from '../../../appStore';
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { useNavigate } from "react-router-dom";
+import { useAppStore } from "../../../appStore";
 
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import CarRentalIcon from '@mui/icons-material/CarRental';
-import RecommendIcon from '@mui/icons-material/Recommend';
-import MessageIcon from '@mui/icons-material/Message';
-import ForumIcon from '@mui/icons-material/Forum';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import PendingActionsIcon from '@mui/icons-material/PendingActions';
-const drawerWidth = 300;
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import CarRentalIcon from "@mui/icons-material/CarRental";
+import RecommendIcon from "@mui/icons-material/Recommend";
+import MessageIcon from "@mui/icons-material/Message";
+import ForumIcon from "@mui/icons-material/Forum";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import PendingActionsIcon from "@mui/icons-material/PendingActions";
+const drawerWidth = 350;
 
 const openedMixin = (theme) => ({
   width: drawerWidth,
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.enteringScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
 });
 
 const closedMixin = (theme) => ({
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
   width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
+  [theme.breakpoints.up("sm")]: {
     width: `calc(${theme.spacing(8)} + 1px)`,
   },
 });
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
 }));
 
 const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
   width: drawerWidth,
   flexShrink: 0,
-  whiteSpace: 'nowrap',
-  boxSizing: 'border-box',
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
   ...(open && {
     ...openedMixin(theme),
-    '& .MuiDrawer-paper': openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
   }),
   ...(!open && {
     ...closedMixin(theme),
-    '& .MuiDrawer-paper': closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
   }),
 }));
 
@@ -80,13 +80,13 @@ export default function Sidebar() {
   const open = useAppStore((state) => state.dopen);
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <Box height={50} />
-      <Drawer variant='permanent' open={open}>
+      <Drawer variant="permanent" open={open}>
         <DrawerHeader>
           <IconButton>
-            {theme.direction === 'rtl' ? (
+            {theme.direction === "rtl" ? (
               <ChevronRightIcon />
             ) : (
               <ChevronLeftIcon />
@@ -96,29 +96,29 @@ export default function Sidebar() {
         <List>
           <ListItem
             disablePadding
-            sx={{ display: 'block' }}
+            sx={{ display: "block" }}
             onClick={() => {
-              navigate('/CustomerDashboard');
+              navigate("/CustomerDashboard");
             }}
           >
             <ListItemButton
               sx={{
                 minHeight: 48,
-                justifyContent: open ? 'initial' : 'center',
+                justifyContent: open ? "initial" : "center",
                 px: 2.5,
               }}
             >
               <ListItemIcon
                 sx={{
                   minWidth: 0,
-                  mr: open ? 3 : 'auto',
-                  justifyContent: 'center',
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
                 }}
               >
                 <DashboardIcon style={{ fontSize: 40 }} />
               </ListItemIcon>
               <ListItemText
-                primary='Dashboard'
+                primary="Dashboard"
                 sx={{ opacity: open ? 1 : 0 }}
               />
             </ListItemButton>
@@ -127,29 +127,29 @@ export default function Sidebar() {
         <List>
           <ListItem
             disablePadding
-            sx={{ display: 'block' }}
+            sx={{ display: "block" }}
             onClick={() => {
-              navigate('/CustomerDashboard/booking');
+              navigate("/CustomerDashboard/booking");
             }}
           >
             <ListItemButton
               sx={{
                 minHeight: 48,
-                justifyContent: open ? 'initial' : 'center',
+                justifyContent: open ? "initial" : "center",
                 px: 2.5,
               }}
             >
               <ListItemIcon
                 sx={{
                   minWidth: 0,
-                  mr: open ? 3 : 'auto',
-                  justifyContent: 'center',
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
                 }}
               >
                 <PendingActionsIcon style={{ fontSize: 40 }} />
               </ListItemIcon>
               <ListItemText
-                primary='Book a service'
+                primary="Book a service"
                 sx={{ opacity: open ? 1 : 0 }}
               />
             </ListItemButton>
@@ -158,29 +158,29 @@ export default function Sidebar() {
         <List>
           <ListItem
             disablePadding
-            sx={{ display: 'block' }}
+            sx={{ display: "block" }}
             onClick={() => {
-              navigate('/CustomerDashboard/register-vehicle');
+              navigate("/CustomerDashboard/register-vehicle");
             }}
           >
             <ListItemButton
               sx={{
                 minHeight: 48,
-                justifyContent: open ? 'initial' : 'center',
+                justifyContent: open ? "initial" : "center",
                 px: 2.5,
               }}
             >
               <ListItemIcon
                 sx={{
                   minWidth: 0,
-                  mr: open ? 3 : 'auto',
-                  justifyContent: 'center',
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
                 }}
               >
                 <CarRentalIcon style={{ fontSize: 40 }} />
               </ListItemIcon>
               <ListItemText
-                primary='Vehicle Registration'
+                primary="Vehicle Registration"
                 sx={{ opacity: open ? 1 : 0 }}
               />
             </ListItemButton>
@@ -189,29 +189,29 @@ export default function Sidebar() {
         <List>
           <ListItem
             disablePadding
-            sx={{ display: 'block' }}
+            sx={{ display: "block" }}
             onClick={() => {
-              navigate('/CustomerDashboard/manufacturer-recommendation');
+              navigate("/CustomerDashboard/manufacturer-recommendation");
             }}
           >
             <ListItemButton
               sx={{
                 minHeight: 48,
-                justifyContent: open ? 'initial' : 'center',
+                justifyContent: open ? "initial" : "center",
                 px: 2.5,
               }}
             >
               <ListItemIcon
                 sx={{
                   minWidth: 0,
-                  mr: open ? 3 : 'auto',
-                  justifyContent: 'center',
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
                 }}
               >
                 <RecommendIcon style={{ fontSize: 40 }} />
               </ListItemIcon>
               <ListItemText
-                primary='Manufacture Recommendation'
+                primary="Manufacturer Recommendation"
                 sx={{ opacity: open ? 1 : 0 }}
               />
             </ListItemButton>
@@ -220,30 +220,30 @@ export default function Sidebar() {
         <List>
           <ListItem
             disablePadding
-            sx={{ display: 'block' }}
+            sx={{ display: "block" }}
             onClick={() => {
-              navigate('/CustomerDashboard/message-system');
+              navigate("/CustomerDashboard/message-system");
             }}
           >
             <ListItemButton
               sx={{
                 minHeight: 48,
-                justifyContent: open ? 'initial' : 'center',
+                justifyContent: open ? "initial" : "center",
                 px: 2.5,
               }}
             >
               <ListItemIcon
                 sx={{
                   minWidth: 0,
-                  mr: open ? 3 : 'auto',
-                  justifyContent: 'center',
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
                 }}
               >
-                {' '}
+                {" "}
                 <MessageIcon style={{ fontSize: 40 }} />
               </ListItemIcon>
               <ListItemText
-                primary='Message System'
+                primary="Message System"
                 sx={{ opacity: open ? 1 : 0 }}
               />
             </ListItemButton>
@@ -252,59 +252,59 @@ export default function Sidebar() {
         <List>
           <ListItem
             disablePadding
-            sx={{ display: 'block' }}
+            sx={{ display: "block" }}
             onClick={() => {
-              navigate('/CustomerDashboard/forum');
+              navigate("/CustomerDashboard/forum");
             }}
           >
             <ListItemButton
               sx={{
                 minHeight: 48,
-                justifyContent: open ? 'initial' : 'center',
+                justifyContent: open ? "initial" : "center",
                 px: 2.5,
               }}
             >
               <ListItemIcon
                 sx={{
                   minWidth: 0,
-                  mr: open ? 3 : 'auto',
-                  justifyContent: 'center',
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
                 }}
               >
-                {' '}
+                {" "}
                 <ForumIcon style={{ fontSize: 40 }} />
               </ListItemIcon>
-              <ListItemText primary='Forum' sx={{ opacity: open ? 1 : 0 }} />
+              <ListItemText primary="Forum" sx={{ opacity: open ? 1 : 0 }} />
             </ListItemButton>
           </ListItem>
         </List>
         <List>
           <ListItem
             disablePadding
-            sx={{ display: 'block' }}
+            sx={{ display: "block" }}
             onClick={() => {
               localStorage.clear();
-              navigate('/');
+              navigate("/");
             }}
           >
             <ListItemButton
               sx={{
                 minHeight: 48,
-                justifyContent: open ? 'initial' : 'center',
+                justifyContent: open ? "initial" : "center",
                 px: 2.5,
               }}
             >
               <ListItemIcon
                 sx={{
                   minWidth: 0,
-                  mr: open ? 3 : 'auto',
-                  justifyContent: 'center',
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
                 }}
               >
-                {' '}
+                {" "}
                 <ExitToAppIcon style={{ fontSize: 40 }} />
               </ListItemIcon>
-              <ListItemText primary='Log out' sx={{ opacity: open ? 1 : 0 }} />
+              <ListItemText primary="Log out" sx={{ opacity: open ? 1 : 0 }} />
             </ListItemButton>
           </ListItem>
         </List>
