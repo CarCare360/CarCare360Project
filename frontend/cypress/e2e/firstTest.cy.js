1fa581d3cd0e9e3405f6681f72e201f462d34d46
Kavindu Chinthana <119396480+kgchinthana@users.noreply.github.com>
2023-10-31 09:42:21 +0530

    add test

--- frontend/cypress/e2e/firstTest.cy.js
+++ frontend/cypress/e2e/firstTest.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000/login')
+    cy.visit('https://car-care-360-jl9d.onrender.com/#/login')
 
     cy.get('[data-testid="cypress-title"]').should("exist")
 
